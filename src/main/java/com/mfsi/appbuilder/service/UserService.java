0ceb509d77d64e7e729d6ad3d5df33cdc836e91f
Nayan <nayana@mindfiresolutions.com>
2019-05-23 12:26:15 +0530

    Adding JWT Authentication

---
+++ src/main/java/com/mfsi/appbuilder/service/UserService.java
@@ -0,0 +1,22 @@
+package com.mfsi.appbuilder.service;
+
+
+import com.mfsi.appbuilder.model.User;
+import com.mfsi.appbuilder.model.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+
+    User save(UserDto user);
+
+    List<User> findAll();
+
+    void delete(int id);
+
+    User findOne(String username);
+
+    User findById(int id);
+
+    UserDto update(UserDto userDto);
+}
---
+++ src/main/java/com/mfsi/appbuilder/service/UserServiceImpl.java
@@ -0,0 +1,94 @@
+package com.mfsi.appbuilder.service;
+
+
+import com.mfsi.appbuilder.model.User;
+import com.mfsi.appbuilder.model.UserDto;
+import com.mfsi.appbuilder.repository.UserDao;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service(value = "userService")
+public class UserServiceImpl implements UserDetailsService, UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private BCryptPasswordEncoder bcryptEncoder;
+
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.print(bcryptEncoder.encode("Abcd@1234"));
+        User user = userDao.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAuthority());
+    }
+
+    private List<SimpleGrantedAuthority> getAuthority() {
+//		System.out.print(roles.stream().map(role -> new SimpleGrantedAuthority(role.getRole())).collect(Collectors.toList()));
+//		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRole())).collect(Collectors.toList());
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        Iterable<User> userList = userDao.findAll();
+        //userDao.findAll().iterator().forEachRemaining(list::add);
+        userList.forEach(list::add);
+
+        return list;
+    }
+
+    @Override
+    public void delete(int id) {
+        userDao.deleteById(id);
+    }
+
+    @Override
+    public User findOne(String username) {
+        return userDao.findByUsername(username);
+    }
+
+    @Override
+    public User findById(int id) {
+        Optional<User> optionalUser = userDao.findById(id);
+        return optionalUser.isPresent() ? optionalUser.get() : null;
+    }
+
+    @Override
+    public UserDto update(UserDto userDto) {
+        User user = findById(userDto.getId());
+        if (user != null) {
+            BeanUtils.copyProperties(userDto, user, "password");
+            userDao.save(user);
+        }
+        return userDto;
+    }
+
+    @Override
+    public User save(UserDto user) {
+        User newUser = new User();
+        newUser.setUsername(user.getUsername());
+        newUser.setFirstName(user.getFirstName());
+        newUser.setLastName(user.getLastName());
+        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+        newUser.setAge(user.getAge());
+        newUser.setSalary(user.getSalary());
+        return userDao.save(newUser);
+    }
+}
