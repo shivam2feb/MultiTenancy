9092bf4bed42ca9bbd243938c043daab689eb91f
Nayan <nayana@mindfiresolutions.com>
2019-07-01 12:50:19 +0530

    Removing extra Attributes from User class and its usage

--- src/main/java/com/mfsi/appbuilder/service/UserServiceImpl.java
+++ src/main/java/com/mfsi/appbuilder/service/UserServiceImpl.java
@@ -87,8 +87,6 @@ public class UserServiceImpl implements UserDetailsService, UserService {
         newUser.setFirstName(user.getFirstName());
         newUser.setLastName(user.getLastName());
         newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
-        newUser.setAge(user.getAge());
-        newUser.setSalary(user.getSalary());
         return userDao.save(newUser);
     }
 }
